1f3ed1721de2f731ac2da4e629039984d0fb2450
Anil Maharjan <simpleanilcha@gmail.com>
2023-04-20 07:18:42 +0545

    creating the HostLayout

--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
 
 export default function Dashboard() {
   return (
     <>
       <h1>Host Dashboard here</h1>
-      <Outlet />
     </>
   )
 }
